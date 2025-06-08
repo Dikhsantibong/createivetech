a8263814745919ba9f52a714603c219a67078689
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-06-08 12:00:10 +0800

    perbaikan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Gp Bootstrap gp</title>
+  <title>Ctech</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
