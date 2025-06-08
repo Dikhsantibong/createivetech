1619f1a5a19a753de107a9445ae4f0b049b4661b
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-06-08 10:52:57 +0800

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,3 @@ use App\Http\Controllers\ServiceController;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('/portfolio/{type}', [PortfolioController::class, 'show']);
-
-Route::get('/layanan/pengembangan-web-dan-mobile', [ServiceController::class, 'mobile']);
-Route::get('/layanan/konsultasi-it', [ServiceController::class, 'consulting']);
-Route::get('/layanan/branding', [ServiceController::class, 'branding']);
-Route::get('/layanan/desain-grafis', [ServiceController::class, 'desain']);
-Route::get('/layanan/editing-video', [ServiceController::class, 'editing']);
-Route::get('/layanan/model-3d', [ServiceController::class, 'animasi']);
-Route::get('/layanan/animator', [ServiceController::class, 'animasi']);
