6d1dc56e324af131eaa044498572e4927776ea91
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-06-09 23:40:41 +0800

    perbaiki

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1030,7 +1030,7 @@
               </div>
               <div class="member-info">
                 <h4>Diky Ardikawiratama</h4>
-                <span>Tech Lead</span>
+                <span>Founder</span>
               </div>
             </div>
           </div><!-- End Team Member -->
